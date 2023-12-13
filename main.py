773d861f2c5e88f22eafcc0171f15d34db9e7d33
barbashovtd <barbashovtd@mail.ru>
2023-12-13 06:32:29 +0300

    base class

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numexpr import evaluate as compute
+from PIL import Image
+from skimage.metrics import structural_similarity as ssim
+
+
+class Dither:
+    def __init__(self) -> None:
+        pass
+
+    def bayer(self):
+        pass
+
+    def fs(self):
+        pass
+
+    def atkinson(self):
+        pass
+
+    def dither(self):
+        pass
+
+    def compare(self):
+        pass
